2bcae78ede939bbeaf139b7eee963ba370cb38ac
logeshwaransj <sjlogeshwaran02@gmail.com>
2024-01-31 19:45:04 +0530

    Update TestDataConstants.java
    updated username and password fields

--- src/constants/TestDataConstants.java
+++ src/constants/TestDataConstants.java
@@ -3,7 +3,7 @@ package constants;
 public class TestDataConstants {
 	
 	// Login Credentials
-	public static String username = "sjlogeshwaran02@gmail.com";
-	public static String password = "Logesh@123";
+	public static String username = "MyMailId";
+	public static String password = "Password";
 
 }
